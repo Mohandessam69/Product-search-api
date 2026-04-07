f95397bac6d744cb81b5ec816643b994b7796219
Mohandessam69 <mohandessam99me@gmail.com>
2026-04-07 23:02:31 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,24 +1,12 @@
 export default async function handler(req, res) {
-
-  const query = req.query.query || "";
-
-  if (!query) {
-    return res.status(200).json([]);
-  }
-
-  try {
-    const response = await fetch(
-      `https://serpapi.com/search.json?engine=google_shopping&q=${query}&api_key=YOUR_API_KEY`
-    );
-
-    const data = await response.json();
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json");
-
-    return res.status(200).json(data.shopping_results || []);
-
-  } catch (error) {
-    return res.status(200).json([]);
-  }
+  const { query } = req.query;
+  
+  const response = await fetch(
+    `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(query)}&api_key=${process.env.SERPAPI_KEY}`
+  );
+  
+  const data = await response.json();
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(data.shopping_results || []);
 }
